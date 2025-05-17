96faff0b0a8030ba179c969cf2e9676fc7944a8b
Henrik Lidbjörk <henrik.lidbjork@readspeaker.com>
2025-05-17 23:18:25 +0200

    Artist discography can now be retrieved.

--- app/Main.hs
+++ app/Main.hs
@@ -6,10 +6,11 @@ module Main where
 import Options.Applicative
 import Network.Wreq (getWith, defaults, params, param, responseBody)
 import Control.Lens
+import qualified Data.Text as T
 import Data.Text.Internal (Text)
 import Data.Aeson.Lens -- (_String, key)
 import Data.Aeson
-import Data.Scientific
+import Data.Maybe (listToMaybe)
 
 -- This app's modules
 import RatingParser
@@ -48,10 +49,10 @@ wikipediaApiUrl = "https://en.wikipedia.org/w/api.php"
 
 -- TODO: Handle several results, like different http response codes etc.
 
-requestWikiParse :: Text -> IO (Maybe Text)
-requestWikiParse page = do
-    let urlParams = [ ("action", "parse"), ("format", "json"), ("prop", "wikitext"), ("redirects", "1") ]
-    let opts = defaults & params .~ urlParams & param "page" .~ [page]
+requestWikiParse :: Text -> Text -> IO (Maybe Text)
+requestWikiParse pageprop val = do
+    let urlParams = [ ("action", "parse"), ("format", "json"), ("prop", "wikitext"), ("redirects", "1"), (pageprop, val) ]
+    let opts = defaults & params .~ urlParams -- & param "page" .~ [val]
     r <- getWith opts wikipediaApiUrl
     return $ r ^? responseBody . key "parse" . key "wikitext" . key "*" . _String
 
@@ -71,14 +72,12 @@ requestWikiSearch searchQuery = do
     -- Example of how to get pageid of first search hit:
     -- r ^. responseBody ^.. _Value . key "query" . key "search" . nth 0 . key "pageid"
 
-findDiscography :: [Value] -> [Text]  -- [[Scientific]]
-findDiscography [] = []
+findDiscography :: [Value] -> Maybe Integer
+findDiscography [] = Nothing
 findDiscography (x:xs) =
-    (x ^. key "title" . _String) : findDiscography xs
-    -- (x ^.. key "pageid" . _Number) : findDiscography xs   -- Don't know why ._Number needs ^.. instead of ^.
-    --
--- TODO: Apply ^.. values and give as arg to this function, and let it map through them (x:xs ...) to find
--- the page that has "discography" in it (maybe literally artist ++ " discography")
+    if T.isSuffixOf "discography" (x ^. key "title" . _String)
+        then (x ^.. key "pageid" . _Integer & listToMaybe)  -- Don't know why ._Integer needs ^.. instead of ^.
+        else findDiscography xs
 
 
 main :: IO ()
@@ -88,13 +87,21 @@ main = do
     let artistName = optArtist inputargs
     case (albumTitle, artistName) of  -- TODO: Maybe if-statement is better?
         (album, "") -> do
-               wikitext <- requestWikiParse album
+               wikitext <- requestWikiParse "page" album  -- TODO: Add searching before parsing
                case wikitext of
-                   Nothing -> putStrLn $ show $ "Failed to fetch wikipedia page for '" <> album <> "'"
+                   Nothing -> print $ "Failed to fetch wikipedia page for '" <> album <> "'"
                    Just w -> getAlbumRatings w albumTitle
         ("", artist) -> do
                wikiresults <- requestWikiSearch artist
                case wikiresults of
-                   Nothing -> putStrLn $ show $ "Search request to wikipedia failed for '" <> artist <> "'"
-                   Just wr -> putStrLn $ show $ findDiscography $ wr ^.. values  -- Maybe move ^..values to requestWikiSearch?
+                   Nothing -> print $ "Search request to wikipedia failed for '" <> artist <> "'"
+                   Just wr -> do
+                       let discoid = findDiscography $ wr ^.. values  -- Maybe move ^..values to requestWikiSearch?
+                       case discoid of
+                           Nothing -> print $ "Could not find discography related to search query '" <> artist <> "'"
+                           Just did -> do
+                               disco <- requestWikiParse "pageid" $ T.pack $ show did
+                               case disco of
+                                   Nothing -> putStrLn "Failed to fetch discography"
+                                   Just d -> print d
         (_, _) -> putStrLn "No album title or artist/band specified."
