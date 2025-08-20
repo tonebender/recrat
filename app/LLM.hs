882963e0200a093495a7e9215f03b45e8a4d63c6
Henrik Lidbjörk <henrik.lidbjork@readspeaker.com>
2025-08-20 16:17:27 +0200

    Adjust spaces in llm schema string.

--- app/LLM.hs
+++ app/LLM.hs
@@ -9,30 +9,29 @@ import Data.Text.Internal (Text)
 llmSchema :: Text
 llmSchema = " \
     \ { \
-      \ \"$schema\": \"http://json-schema.org/draft-07/schema#\", \
-      \ \"type\": \"object\", \
-      \ \"properties\": { \
-        \ \"albums\": { \
-          \ \"type\": \"array\", \
-          \ \"items\": { \
-            \ \"type\": \"object\", \
-            \ \"properties\": { \
-              \ \"title\": { \
-                \ \"type\": \"string\" \
-              \ }, \
-              \ \"year\": { \
-                \ \"type\": \"string\" \
-              \ }, \
-              \ \"description\": { \
-                \ \"type\": \"string\" \
-              \ } \
-            \ }, \
-            \ \"required\": [\"title\", \"year\", \"description\"], \
-            \ \"additionalProperties\": false \
-          \ } \
-        \ } \
-      \ }, \
-      \ \"required\": [\"albums\"], \
-      \ \"additionalProperties\": false \
-    \ } \
-\ "
+    \   \"$schema\": \"http://json-schema.org/draft-07/schema#\", \
+    \   \"type\": \"object\", \
+    \   \"properties\": { \
+    \     \"albums\": { \
+    \       \"type\": \"array\", \
+    \       \"items\": { \
+    \         \"type\": \"object\", \
+    \         \"properties\": { \
+    \           \"title\": { \
+    \             \"type\": \"string\" \
+    \           }, \
+    \           \"year\": { \
+    \             \"type\": \"string\" \
+    \           }, \
+    \           \"description\": { \
+    \             \"type\": \"string\" \
+    \           } \
+    \         }, \
+    \         \"required\": [\"title\", \"year\", \"description\"], \
+    \         \"additionalProperties\": false \
+    \       } \
+    \     } \
+    \   }, \
+    \   \"required\": [\"albums\"], \
+    \   \"additionalProperties\": false \
+    \ }"
