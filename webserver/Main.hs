aaafdd5fd6e27e48f28e4a05895ade9dd43c5e82
Henrik Lidbjörk <henrik.lidbjork@readspeaker.com>
2025-10-05 22:30:29 +0200

    webserver: add blaze-html.

--- webserver/Main.hs
+++ webserver/Main.hs
@@ -2,8 +2,14 @@
 
 module Main where
 
-import Web.Scotty
+import Web.Scotty as S
+import Text.Blaze.Html5 as H
+import Text.Blaze.Html5.Attributes as A
 
 main :: IO ()
 main = scotty 3000 $ do
-    get "/" $ text "Hello Scotty!"
+    get "/" $ do
+        S.text "Index page"
+    get "/search" $ do
+        artist <- queryParam "artist"
+        S.html $ mconcat ["<h1>", artist, "</h1>"]
