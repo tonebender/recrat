b0d398687e01d50341d5cd97dce389b5a206fb3c
Henrik Lidbjörk <henrik.lidbjork@readspeaker.com>
2025-10-09 23:20:16 +0200

    webserver: replace blaze-hmtl with lucid2.

--- webserver/Main.hs
+++ webserver/Main.hs
@@ -1,15 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
 
 module Main where
 
 import Web.Scotty as S
-import Text.Blaze.Html5 as H
-import Text.Blaze.Html5.Attributes as A
+import Data.Text (Text)
+import Lucid
 
 main :: IO ()
 main = scotty 3000 $ do
     get "/" $ do
         S.text "Index page"
     get "/search" $ do
-        artist <- queryParam "artist"
-        S.html $ mconcat ["<h1>", artist, "</h1>"]
+        artist <- S.queryParam "artist" :: ActionM Text
+        S.html $ renderText (p_ (toHtml ("Hello " <> artist)))
