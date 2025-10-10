39f671a6ca9f7864f322000ea541e77cf50b471b
Henrik Lidbjörk <henrik.lidbjork@readspeaker.com>
2025-10-10 16:21:32 +0200

    webserver: Implement handling of some basic URL queries.

--- webserver/Main.hs
+++ webserver/Main.hs
@@ -3,14 +3,28 @@
 
 module Main where
 
-import Web.Scotty as S
-import Data.Text (Text)
+import qualified Web.Scotty as S
+import Data.Text.Lazy (Text)
 import Lucid
 
 main :: IO ()
-main = scotty 3000 $ do
-    get "/" $ do
-        S.text "Index page"
-    get "/search" $ do
-        artist <- S.queryParam "artist" :: ActionM Text
-        S.html $ renderText (p_ (toHtml ("Hello " <> artist)))
+main = S.scotty 3000 $ do
+    S.get "/" $ do
+        maybeArtist <- S.queryParamMaybe "artist"
+        maybeLLM <- S.queryParamMaybe "llm"
+        maybeWiki <- S.queryParamMaybe "wikipedia"
+        case (maybeArtist, maybeLLM, maybeWiki) of
+            (Nothing, _, _) -> S.html indexPage  -- No artist specified; show index page. TODO: Add parameter for error msg
+            (_, Nothing, Nothing) -> S.html "Neither wiki nor LLM chosen!"
+            (Just artist, maybeWiki, maybeLLM) -> do
+                t <- S.liftIO $ runQuery artist (maybeWiki, maybeLLM)
+                S.html t
+                -- S.html $ renderText (p_ (toHtml ("Hello " <> artist)))
+
+-- This is where we're supposed to do our business logic with Wiki/LLM
+runQuery :: Text -> (Maybe Text, Maybe Text) -> IO (Text)
+runQuery artist (maybeWiki, maybeLLM) = return $ "Okay, " <> artist
+
+-- TODO: This should take a parameter for a flash message or so
+indexPage :: Text
+indexPage = "Please specify an artist"
