01074dbb5c4a0fa83a0a169eccfce743d12d31e2
Henrik Lidbjörk <henrik.lidbjork@readspeaker.com>
2025-10-11 23:51:47 +0200

    webserver: further work ...

--- webserver/Main.hs
+++ webserver/Main.hs
@@ -7,6 +7,9 @@ import qualified Web.Scotty as S
 import Data.Text.Lazy (Text)
 import Lucid
 
+import Wiki.Artist (fetchArtist)
+import LLM.LLM (llmFetchArtist)
+
 main :: IO ()
 main = S.scotty 3000 $ do
     S.get "/" $ do
@@ -14,17 +17,23 @@ main = S.scotty 3000 $ do
         maybeLLM <- S.queryParamMaybe "llm"
         maybeWiki <- S.queryParamMaybe "wikipedia"
         case (maybeArtist, maybeLLM, maybeWiki) of
-            (Nothing, _, _) -> S.html indexPage  -- No artist specified; show index page. TODO: Add parameter for error msg
-            (_, Nothing, Nothing) -> S.html "Neither wiki nor LLM chosen!"
-            (Just artist, maybeWiki, maybeLLM) -> do
-                t <- S.liftIO $ runQuery artist (maybeWiki, maybeLLM)
+            (Nothing, _, _) -> S.html $ indexPage "No artist specified"
+            (_, Nothing, Nothing) -> S.html "Neither Wikipedia nor AI chosen!"
+            (Just artist, maybeW, maybeL) -> do
+                t <- S.liftIO $ runQuery artist (maybeW, maybeL)
                 S.html t
                 -- S.html $ renderText (p_ (toHtml ("Hello " <> artist)))
 
 -- This is where we're supposed to do our business logic with Wiki/LLM
 runQuery :: Text -> (Maybe Text, Maybe Text) -> IO (Text)
-runQuery artist (maybeWiki, maybeLLM) = return $ "Okay, " <> artist
+runQuery artist (maybeWiki, maybeLLM) = do
+    let wikiResult = case maybeWiki of
+            Nothing -> ""
+            Just w -> w
+    let llmResult = case maybeLLM of
+            Nothing -> ""
+            Just l -> l
+    return $ "Okay, " <> artist <> " with " <> wikiResult <> " " <> llmResult
 
--- TODO: This should take a parameter for a flash message or so
-indexPage :: Text
-indexPage = "Please specify an artist"
+indexPage :: Text -> Text
+indexPage message = "Please specify an artist"
