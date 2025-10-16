bb5056b43327a9ae0bab17894976082abc4e7c11
Henrik Lidbjörk <henrik.lidbjork@readspeaker.com>
2025-10-16 11:36:36 +0200

    webserver: fix strict/lazy text types

--- webserver/Main.hs
+++ webserver/Main.hs
@@ -5,11 +5,12 @@
 module Main where
 
 import qualified Web.Scotty as S
-import Data.Text.Lazy (Text)
-import qualified Data.Text.Lazy as T (toStrict, fromStrict, length)
+import Data.Text (Text)
+import Data.Text.Lazy (LazyText, toStrict, fromStrict)
+import qualified Data.Text as T (length)
 import Lucid
 
-import qualified Wiki.Artist as W (fetchArtist, showArtist, Artist(..), ArtistError2 (ArtistError2))
+import qualified Wiki.Artist as W (fetchArtist, Artist(..), ArtistError2 (ArtistError2))
 import qualified LLM.LLM as L (fetchArtist, showArtist, Artist(..))
 
 main :: IO ()
@@ -27,43 +28,43 @@ main = S.scotty 3000 $ do
             ("", w, l) -> S.html $ indexPage "No artist specified" "" w l
             (artist', False, False) -> S.html $ indexPage "Please choose Wikipedia and/or Mistral AI sources" artist' False False
             (artist', wiki', llm') -> do
-                S.html =<< S.liftIO (runQuery artist' wiki' llm')
+                S.html =<< S.liftIO (handleRequest artist' wiki' llm')
 
 -- | Check if a list of query parameters has a certain parameter.
 -- Returns True if it does, False otherwise. Ignores the value of the param.
 hasQueryParam :: [S.Param] -> Text -> Bool
-hasQueryParam paramList paramName = filter (\(p, _) -> p == T.toStrict paramName) paramList /= []
+hasQueryParam paramList paramName = filter (\(p, _) -> p == paramName) paramList /= []
 
 -- | Take an artist name and a boolean each for wiki and LLM and call the right backend
 -- functions for retrieving artists
-runQuery :: Text -> Bool -> Bool -> IO (Text)
-runQuery artist wiki llm = do
+handleRequest :: Text -> Bool -> Bool -> IO (LazyText)
+handleRequest artist wiki llm = do
     wikiResult <- case wiki of
             False -> return ""
             True -> do
-                eitherArtist <- W.fetchArtist (T.toStrict artist) "studio"
+                eitherArtist <- W.fetchArtist artist "studio"
                 case eitherArtist of
                     Left (W.ArtistError2 t) -> return t
-                    Right artistObj -> return $ W.showArtist artistObj "" True
+                    Right artistObj -> return $ wikiArtistToHtml artistObj
     llmResult <- case llm of
             False -> return ""
             True -> do
-                eitherLlmArtist <- L.fetchArtist (T.toStrict artist) "studio"
+                eitherLlmArtist <- L.fetchArtist artist "studio"
                 case eitherLlmArtist of
                     Left t -> return t
                     Right llmArtistObj -> return $ L.showArtist llmArtistObj
-    return $ artist <> "<br>" <> (T.fromStrict wikiResult) <> "<br>" <> (T.fromStrict llmResult)
+    return $ fromStrict $ artist <> "<br>" <> wikiResult <> "<br>" <> llmResult
 
 wikiArtistToHtml :: W.Artist -> Text
-wikiArtistToHtml artist = renderText $ doctypehtml_ $ do
+wikiArtistToHtml artist = toStrict . renderText . doctypehtml_ $ do
     div_ $ toHtml artist.name
 
 llmArtistToHtml :: L.Artist -> Text
-llmArtistToHtml artist = renderText $ doctypehtml_ $ do
+llmArtistToHtml artist = toStrict . renderText . doctypehtml_ $ do
     div_ $ toHtml artist.name
 
-indexPage :: Text -> Text -> Bool -> Bool -> Text
-indexPage message artist wiki llm = renderText $ doctypehtml_ $ do
+indexPage :: Text -> Text -> Bool -> Bool -> LazyText
+indexPage message artist wiki llm = renderText . doctypehtml_ $ do
     head_ $ title_ "Rec Rat"
     body_ $ do
         h1_ "Rec Rat"
@@ -71,7 +72,7 @@ indexPage message artist wiki llm = renderText $ doctypehtml_ $ do
         form_ [autocomplete_ "off"] $ do
             div_ $ do
                 label_ [for_ "artistInput"] "Artist"
-                input_ [id_ "artistInput", name_ "artist", value_ (T.toStrict artist)]
+                input_ [id_ "artistInput", name_ "artist", value_ artist]
             div_ $ do
                 label_ (do input_ ([type_ "checkbox", id_ "wikiCheck", name_ "wikipedia"] ++ [checked_ | wiki]); "Wikipedia")
                 label_ (do input_ ([type_ "checkbox", id_ "llmCheck", name_ "llm"] ++ [checked_ | llm]); "Mistral AI")
